c4ec422266a96fc7c52b2a95ce2f510fe629a896
Remco Hendriksen <hendriksenremco@hotmail.com>
2020-11-04 23:38:01 +0100

    Robots.txt

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,11 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
+        name: 'robots',
+        content:
+          'index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1',
+      },
+      {
         hid: 'description',
         name: 'description',
         content:
