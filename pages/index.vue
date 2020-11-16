24ff9c3f43ace3b0730f589a70dd543ed9d0fa00
Remco Hendriksen <hendriksenremco@hotmail.com>
2020-11-16 18:16:25 +0100

    Intersection observer

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,7 @@
     >
       {{ article.fields.description }}
     </article-summary>
+    <div ref="end"></div>
   </div>
 </template>
 
@@ -23,14 +24,48 @@ export default {
     const articles = await client.getEntries({
       content_type: 'blogPost',
       order: '-sys.createdAt',
+      limit: 3,
     })
 
     return {
       articles,
     }
   },
+  data() {
+    return {
+      end: false,
+    }
+  },
+
+  mounted() {
+    const intersectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            this.loadMore()
+          }
+        })
+      },
+      { threshold: 1 }
+    )
+    intersectionObserver.observe(this.$refs.end)
+  },
 
   methods: {
+    async loadMore() {
+      if (this.end) return false
+      const result = await client.getEntries({
+        content_type: 'blogPost',
+        order: '-sys.createdAt',
+        skip: this.articles.items.length,
+        limit: 3,
+      })
+      console.log('Load moore')
+      this.articles.items = [...this.articles.items, ...result.items]
+      if (result.total <= this.articles.items.length) {
+        this.end = true
+      }
+    },
     author(authorSlug) {
       return this.authors.find((author) => {
         return author.slug === authorSlug
