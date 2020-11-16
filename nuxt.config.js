fb095662d4d57a5072de3b421c10e158095046c8
Remco Hendriksen <hendriksenremco@hotmail.com>
2020-11-16 16:05:35 +0100

    Adsense code

--- nuxt.config.js
+++ nuxt.config.js
@@ -113,7 +113,15 @@ export default {
         href: `https://vuescratch.com/`,
       },
     ],
-    script: [homeSchemaOrg()],
+    script: [
+      homeSchemaOrg(),
+      {
+        hid: 'adsense',
+        'data-ad-client': 'ca-pub-4674696175162885',
+        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+        async: true,
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
