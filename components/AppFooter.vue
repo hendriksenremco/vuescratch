76d8cc3bac5fce2c712682c49ac77cb580295426
Remco Hendriksen <hendriksenremco@hotmail.com>
2020-11-20 12:16:46 +0100

    Privacy page

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -3,19 +3,41 @@
     <div class="app-footer__socials">
       <socials />
     </div>
+    <div class="app-footer__links">
+      VueScratch&copy; 2020 -
+      <nuxt-link :to="{ name: 'privacy' }">Privacy</nuxt-link>
+    </div>
   </footer>
 </template>
 <style lang="scss">
 .app-footer {
   border-top: 1px solid $grey-darker;
-  padding: $spacing * 8;
+  color: $grey-lighter;
 
   &__socials {
-    color: $grey-lighter;
     max-width: $breakpoint-mobile;
     margin: 0 auto;
     text-align: center;
     width: 100%;
+    padding: $spacing * 8;
+  }
+
+  &__links {
+    font-size: 0.8rem;
+    margin: 0 auto;
+    max-width: $breakpoint-mobile;
+    text-align: center;
+    padding: $spacing * 4;
+    width: 100%;
+
+    a {
+      color: inherit;
+      text-decoration: none;
+
+      &:hover {
+        color: $white;
+      }
+    }
   }
 }
 </style>
---
+++ pages/privacy.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container" data-theme="light">
+    <h1>{{ page.fields.title }}</h1>
+    <article-content v-html="markedBody">
+      {{ markedBody }}
+    </article-content>
+  </div>
+</template>
+<script>
+import marked from 'marked'
+import { createClient } from '~/plugins/contentful.js'
+export default {
+  async asyncData() {
+    const client = createClient()
+    const pages = await client.getEntries({
+      content_type: 'page',
+      'fields.slug': 'privacy',
+    })
+
+    return { page: pages.items[0] }
+  },
+  computed: {
+    markedBody() {
+      return marked(this.page.fields.body)
+    },
+  },
+}
+</script>
